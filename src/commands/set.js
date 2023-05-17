d77f6a68a88980cfe697052519938d2938976428
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-17 11:27:19 -0500

    Create command set

---
+++ src/commands/set.js
@@ -0,0 +1,30 @@
+var { psc, bot } = require('../../index.js');
+const { Soup } = require('stews');
+
+async function data(ctx, cmd) {
+    let stuff = require('../data/stuff.js').load();
+    let file = "./src/data/stuff.json");
+    
+    let index = cmd.args[0];
+    cmd.args.shift();
+    let j = cmd.args.join(" ");
+  
+    
+    if (!parseInt(index)+1) {
+        return psc.reply("you have to put an index to set dumbass", { deleteAfter: "2s" });
+    }
+    if (parseInt(index) < 0) {
+        return psc.reply("you can't set an index that's less than 0 dumbass", { deleteAfter: "2s" });
+    }
+    if (stuff.includes(j)) {
+        return psc.reply("that's already in there dumbass", { deleteAfter: "2s" });
+    }
+    else stuff.set(index, j);
+    stuff.dump(file, null, 4);
+    
+    
+    return ctx.reply(`set your shit at to `+"`"+j+"`");
+}
+
+
+psc.command({ name: "set", aliases: "edit" }, data);
