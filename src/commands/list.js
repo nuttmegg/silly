0f30c778d1e2233d57a2c09d038ae5c1c16c34f8
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-17 12:11:51 -0500

    Create list.js

---
+++ src/commands/list.js
@@ -0,0 +1,25 @@
+var { psc, bot } = require('../../index.js');
+var { Paged } = require('../classes');
+const { Soup } = require('stews');
+
+async function data(ctx, cmd) {
+    let stuff = require('../data/stuff.js').load();
+    let page = 1;
+  
+    let paged = new Paged(stuff);
+    
+    if (paged.total <= 0) {
+        return ctx.reply("there are none rn come back later ig and maybe there will be stuff we'll see yeah");
+    }
+    
+    let bigleft = new psc.Button({ id: "stewd/bigleft", label: "big left", style: "secondary" });
+    let left = new psc.Button({ id: "stewd/left", label: "left", style: "secondary" });
+    let right = new psc.Button({ id: "stewd/right", label: "right", style: "secondary" });
+    let bigright = new psc.Button({ id: "stewd/bigright", label: "big right", style: "secondary" });
+    
+    let row = new psc.ActionRow([ bigleft, left, right, bigright ]);
+    
+    return ctx.reply(`page ${page}/${paged.pages} \n\n ${paged.page(page).join("\n")}`, { components: [row] });
+}
+
+psc.command({ name: "list", aliases: ["all", "every"] }, data);
