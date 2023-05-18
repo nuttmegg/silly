11449e7e11fe6644cca5ce4d6bc40cc3e7e28a2a
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-17 21:52:36 -0500

    Update list.js

--- src/commands/list.js
+++ src/commands/list.js
@@ -3,23 +3,28 @@ var { Paged } = require('../classes');
 const { Soup } = require('stews');
 
 async function data(ctx, cmd) {
-    let stuff = require('../data/stuff.js').load();
+    let stuff = require('../data/stuff.js').load().copy();
     let page = 1;
-  
-    let paged = new Paged(stuff);
-    
+
+	stuff = stuff.map( (v, i) => { return `#${i}:**  **${"`"}${v}${"`"}` })
+
+	
+    let paged = new Paged(5, stuff);
     if (paged.total <= 0) {
         return ctx.reply("there are none rn come back later ig and maybe there will be stuff we'll see yeah");
     }
-    
-    let bigleft = new psc.Button({ id: "stewd/bigleft", label: "big left", style: "secondary" });
-    let left = new psc.Button({ id: "stewd/left", label: "left", style: "secondary" });
-    let right = new psc.Button({ id: "stewd/right", label: "right", style: "secondary" });
-    let bigright = new psc.Button({ id: "stewd/bigright", label: "big right", style: "secondary" });
-    
+
+	
+    let bigleft = new psc.Button({ id: "stewd/bigleft", label: "->> big left", style: "secondary" });
+    let left = new psc.Button({ id: "stewd/left", label: "-> left", style: "secondary" });
+    let right = new psc.Button({ id: "stewd/right", label: "<- right", style: "secondary" });
+    let bigright = new psc.Button({ id: "stewd/bigright", label: " big right <<-", style: "secondary" });
+	
     let row = new psc.ActionRow([ bigleft, left, right, bigright ]);
-    
-    return ctx.reply(`page ${page}/${paged.pages} \n\n ${paged.page(page).join("\n")}`, { components: [row] });
+
+	
+    return psc.reply(`page ${page}/${paged.pages} \n\n ${paged.page(page).join("\n")}`, { components: [row] });
 }
+
 
 psc.command({ name: "list", aliases: ["all", "every"] }, data);
